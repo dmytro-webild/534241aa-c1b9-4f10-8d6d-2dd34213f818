a0f4598d9786e34150383c416e7e39f41dd0db36
dk <kudin.dima@gmail.com>
2026-05-07 01:40:43 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { DM_Sans } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Vision Vault Media | Sleek Web Agency',
+  description: 'Vision Vault Media is a premium web agency creating high-end digital experiences, sleek interfaces, and innovative solutions for visionaries.',
+};
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+});
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${dmSans.variable} ${inter.variable} antialiased`}>
+          
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
